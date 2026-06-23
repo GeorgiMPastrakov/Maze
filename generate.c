231f1384d0cd2c6461c3d2383a08f5b830f6c6e8
GeorgiMPastrakov <georgi.m.pastrakov.2023@elsys-bg.org>
2026-06-23 10:43:28 +0300

    Add recursive backtracker carving

--- generate.c
+++ generate.c
@@ -18,23 +18,35 @@ static int rng_range(Rng *rng, int bound) {
     return (int)(rng_next(rng) % (unsigned)bound);
 }
 
-static void carve_border_path(Maze *maze, Rng *rng) {
+static void shuffle_directions(int *order, Rng *rng) {
     int i;
-    int w = maze->width;
-    int h = maze->height;
-    if (rng_range(rng, 2) == 0) {
-        for (i = 0; i < w; i++) {
-            maze->cells[0][i] = EMPTY;
-        }
-        for (i = 0; i < h; i++) {
-            maze->cells[i][w - 1] = EMPTY;
-        }
-    } else {
-        for (i = 0; i < h; i++) {
-            maze->cells[i][0] = EMPTY;
-        }
-        for (i = 0; i < w; i++) {
-            maze->cells[h - 1][i] = EMPTY;
+    for (i = 0; i < 4; i++) {
+        order[i] = i;
+    }
+    for (i = 3; i > 0; i--) {
+        int j = rng_range(rng, i + 1);
+        int tmp = order[i];
+        order[i] = order[j];
+        order[j] = tmp;
+    }
+}
+
+static void carve(Maze *maze, int row, int col, Rng *rng) {
+    static const int step_row[4] = {-2, 2, 0, 0};
+    static const int step_col[4] = {0, 0, -2, 2};
+    int order[4];
+    int i;
+    maze->cells[row][col] = EMPTY;
+    shuffle_directions(order, rng);
+    for (i = 0; i < 4; i++) {
+        int dir = order[i];
+        int next_row = row + step_row[dir];
+        int next_col = col + step_col[dir];
+        if (next_row >= 0 && next_row < maze->height &&
+            next_col >= 0 && next_col < maze->width &&
+            maze->cells[next_row][next_col] == WALL) {
+            maze->cells[row + step_row[dir] / 2][col + step_col[dir] / 2] = EMPTY;
+            carve(maze, next_row, next_col, rng);
         }
     }
 }
@@ -45,5 +57,6 @@ void generate_maze(Maze *maze, int width, int height, unsigned seed) {
     maze->width = width;
     maze->height = height;
     fill_walls(maze);
-    carve_border_path(maze, &rng);
+    carve(maze, 0, 0, &rng);
+    maze->cells[height - 1][width - 1] = EMPTY;
 }
