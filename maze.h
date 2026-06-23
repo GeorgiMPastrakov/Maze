e81d1e7c9d458e79687b434b9601cf7f4c726b36
GeorgiMPastrakov <georgi.m.pastrakov.2023@elsys-bg.org>
2026-06-23 10:40:39 +0300

    Add maze type and shared constants

---
+++ maze.h
@@ -0,0 +1,32 @@
+#ifndef MAZE_H
+#define MAZE_H
+
+#define MAZE_MIN 3
+#define MAZE_MAX_W 40
+#define MAZE_MAX_H 30
+
+#define WALL '#'
+#define EMPTY ' '
+
+typedef struct {
+    int width;
+    int height;
+    char cells[MAZE_MAX_H][MAZE_MAX_W];
+} Maze;
+
+void fill_walls(Maze *maze);
+void draw_maze(const Maze *maze);
+void draw_maze_marked(const Maze *maze, int row, int col, char glyph);
+void clear_screen(void);
+
+void generate_maze(Maze *maze, int width, int height, unsigned seed);
+
+int write_maze(const Maze *maze, const char *filename);
+int read_maze(Maze *maze, const char *filename);
+
+void player_traverse(const Maze *maze);
+
+void solve_dfs(const Maze *maze);
+void solve_bfs(const Maze *maze);
+
+#endif
